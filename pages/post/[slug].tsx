7e3b1f81814577d4b0000396182400d5553fc6e6
AayushMohan <aayushmohan1702@gmail.com>
2022-01-20 21:56:16 +0530

    Added Portable Text

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -2,9 +2,10 @@ import { GetStaticProps } from "next";
 import Header from "../../components/Header";
 import { sanityClient, urlFor } from "../../sanity";
 import { Post } from "../../typings";
+import PortableText from "react-portable-text";
 
 interface Props {
-  post: [Post];
+  post: Post;
 }
 
 function Post({ post }: Props) {
@@ -36,6 +37,30 @@ function Post({ post }: Props) {
             Published at {""} {new Date(post._createdAt).toLocaleString()}
           </p>
         </div>
+        <div>
+          <PortableText
+            className=""
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="text-2xl font-bold my-5" {...props} />
+              ),
+              h2: (props: any) => (
+                <h2 className="text-xl font-bold my-5" {...props} />
+              ),
+              li: ({ children }: any) => (
+                <li className="ml-4 list-disc">{children}</li>
+              ),
+              link: ({ href, children }: any) => (
+                <a href={href} className="text-blue-500 hover:underline">
+                  {children}
+                </a>
+              ),
+            }}
+          />
+        </div>
       </article>
     </main>
   );
@@ -46,15 +71,17 @@ export default Post;
 export const getStaticPaths = async () => {
   const query = `*[_type == "post"]{
     _id,
-    slug{
-    current
-   }
+    slug {
+      current
+    }
   }`;
 
   const posts = await sanityClient.fetch(query);
 
   const paths = posts.map((post: Post) => ({
-    params: { slug: post.slug.current },
+    params: {
+      slug: post.slug.current,
+    },
   }));
 
   return {
