d2025a06d343be3be529988658ee6356d9ec90d0
AayushMohan <aayushmohan1702@gmail.com>
2022-01-22 16:59:52 +0530

    Added Comments

--- typings.d.ts
+++ typings.d.ts
@@ -6,14 +6,32 @@ export interface Post{
      name: string;
      image: string;
     };
+    comments: Comment[];
     description: string;
     mainImage: {
      asset: {
       url: string;
-     }
-    }
+     };
+    };
     slug: {
      current: string;
     };
     body: [object];
+}
+
+export interface Comment{
+    comment: ReactNode;
+    approved: boolean;
+    comments: string;
+    email: string;
+    name: string;   
+    post: {
+        _ref: string;
+        _type: string;
+    };
+    _createdAt: string;
+    _id: string;
+    _rev: string;
+    _type: string;
+    _updatedAt: string;
 }
